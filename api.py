beb26c0e3b03e3be13d34f77dbcbb6e76a6da148
Dimis <dimisoff@gmail.com>
2024-10-04 16:13:09 +0300

    Подходим к маршрутизации

--- api.py
+++ api.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 async def welcome() -> dict:
     return {"message": "Hello World"}
+
+
+@app.get("/hello/{user}")
+async def welcome_user(user: str) -> dict:
+    return {"user": f'Hello {user}'}
+
+# uvicorn api:app --port 8000 --reload  # используй для запуска
